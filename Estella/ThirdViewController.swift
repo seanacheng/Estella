c3859bc59b4d057d53af5f2100d7c264b7097024
Sean <sean.cheng@intelluslearning.com>
2018-02-06 11:33:04 +0800

    changed tab names

---
+++ Estella/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  Estella
+//
+//  Created by Sean Cheng on 2/6/18.
+//  Copyright © 2018 NTU. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+

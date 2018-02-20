0662da8a4a8ab7b4c4e4b87c04c726afe86918b2
Sean <sean.cheng@intelluslearning.com>
2018-02-20 21:10:36 +0800

    submit button goes to diary

--- Estella/FirstViewController.swift
+++ Estella/FirstViewController.swift
@@ -20,6 +20,10 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goToDiary(_ sender: Any) {
+        tabBarController?.selectedIndex=1
+        
+    }
+    
 }
 

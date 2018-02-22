fadac5d9ba9d52ad01e524c14a0933926e234597
Sean <sean.cheng@intelluslearning.com>
2018-02-22 13:17:58 +0800

    attempt to add a spritekit node

--- Estella/ThirdViewController.swift
+++ Estella/ThirdViewController.swift
@@ -14,12 +14,11 @@ class ThirdViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view = SKView()
+//        let user = SKSpriteNode(imageNamed: "anonymous")
+//        user.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
+//        addChild(user)
     }
     
-    var skView: SKView {
-        return view as! SKView
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

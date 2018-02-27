bbb54d9d995b5a2d975f37829b386c071f4985e8
Sean <sean.cheng@intelluslearning.com>
2018-02-27 21:24:14 +0800

    hardcoded video entry

--- Estella/FourthViewController.swift
+++ Estella/FourthViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class FourthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        playVideo()
 
         // Do any additional setup after loading the view.
     }
@@ -23,5 +26,23 @@ class FourthViewController: UIViewController {
     
     @IBAction func goToDiary(_ sender: Any) {
         tabBarController?.selectedIndex=2
+    }
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "dolphins", ofType:"mov") else {
+            debugPrint("dolphins.mov not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerViewController = AVPlayerViewController()
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        playerViewController.view.frame = CGRect (x:screenWidth*0.15, y:screenHeight*0.15, width:screenWidth*0.7, height:screenHeight*0.6)
+        playerViewController.player = player
+        
+        self.addChildViewController(playerViewController)
+        self.view.addSubview(playerViewController.view)
+        playerViewController.didMove(toParentViewController: self)
+
     }
 }

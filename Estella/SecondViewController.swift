c49c81259ee3303790350c84da74deee60893b40
Sean <sean.cheng@intelluslearning.com>
2018-03-01 13:18:47 +0800

    edited scrollview on diary page

--- Estella/SecondViewController.swift
+++ Estella/SecondViewController.swift
@@ -16,9 +16,7 @@ class SecondViewController: UIViewController {
         let screenWidth = screensize.width
         let screenHeight = screensize.height
         var myScrollView: UIScrollView!
-        myScrollView = UIScrollView(frame: CGRect(x: 0, y: 50, width: screenWidth, height: screenHeight))
-        myScrollView.contentSize = CGSize(width: screenWidth, height: 2000)
-        view.addSubview(myScrollView)
+        myScrollView = UIScrollView(frame: CGRect(x: 0, y: 40, width: screenWidth, height: screenHeight))
 
         let url = URL(string: "http://10.27.168.4:8891/diary")!
         var request = URLRequest(url: url)
@@ -43,22 +41,41 @@ class SecondViewController: UIViewController {
         }
         task.resume()
 
-        let diarys = [["author": "zhihao", "type": "text", "score": 0.7, "content": "This is my first diary"], ["author": "zhihao", "type": "video", "score": 0.6, "video": "first.mp4"], ["author": "zhihao", "type": "text", "score": 0.0, "content": "Kjbhkbkbkj"], ["author": "zhihao", "type": "text", "score": 0.20000000298023224, "content": "Get this data"]]
+        let diarys = [["author": "zhihao", "type": "text", "score": 0.7, "content": "This is my first diary"], ["author": "zhihao", "type": "video", "score": 0.6, "video": "first.mp4"], ["author": "zhihao", "type": "text", "score": 0.1, "content": "This is a long diary that will need to be several lines so I want to make sure that my app can see that."], ["author": "zhihao", "type": "text", "score": 0.20000000298023224, "content": "Get this data"]]
         var startY = 0
+        let labelHeight = 70        // enough space for content and score
+        var contentHeight = 0
         for dict in diarys {
-            let label = UILabel(frame: CGRect(x: 80, y: startY, width: Int(screenWidth), height: 100))
+            let label = UILabel()
+            var labelHeightOffset = 0
             label.textAlignment = .left
             label.lineBreakMode = .byWordWrapping
             label.numberOfLines = 100;
             label.text = ""
             for (key, value) in dict {
-                print("The \(key) is \(value).")
-                label.text = label.text! + "\(value) \n"
+                switch "\(key)" {
+                case "timestamp":
+                    label.text = label.text! + "\(value) \n"
+                case "content":
+                    label.text = label.text! + "\(value) \n"
+                    labelHeightOffset += "\(value)".count/2         // adds enough space to fit the content
+                case "video":
+                    label.text = label.text! + "\(value) \n"        // would like to move this piece above score
+                case "score":
+                    label.text = label.text! + "Score: \(value) \n"
+                default:
+                    continue
+                }
+                print("The \(key) is: \(value).")
             }
             print("-------------------------")
+            label.frame = CGRect(x: 10, y: startY, width: Int(screenWidth), height: labelHeight + labelHeightOffset)
+            contentHeight += labelHeightOffset + 70
             myScrollView.addSubview(label)
-            startY += 100
+            startY += 50 + labelHeightOffset
         }
+        myScrollView.contentSize = CGSize(width: Int(screenWidth), height: contentHeight)
+        view.addSubview(myScrollView)
     }
     
     override func didReceiveMemoryWarning() {
--- Estella/ThirdViewController.swift
+++ Estella/ThirdViewController.swift
@@ -12,10 +12,28 @@ class ThirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //        let user = SKSpriteNode(imageNamed: "anonymous")
-        //        user.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
-        //        addChild(user)
+        let url = URL(string: "http://10.27.168.4:8891/event")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            //            for dict in responseString! {
+            //                print(dict)
+            //            }
+        }
+        task.resume()
     }
     
 
